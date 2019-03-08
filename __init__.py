066f4b039852661ce9561de5bec8c952e6a5e17d
luwt <luwentao@alwaysnb.com>
2019-03-08 15:53:39 +0800

    mybatis generator初版

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+_author_ = 'luwt'
+_date_ = '2019/1/23 11:20'
---
+++ mysql_type.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+_author_ = 'luwt'
+_date_ = '2019/1/23 11:27'
+
+
+class MysqlType(Enum):
+    tinyint = ('TINYINT', 'Integer')
+    int = ('INTEGER', 'Integer')
+    text = ('VARCHAR', 'String')
+    float = ('FLOAT', 'Float')
+    double = ('DOUBLE', 'Double')
+    decimal = ('DECIMAL', 'BigDecimal')
+    date = ('DATE', 'Date')
+    datetime = ('TIMESTAMP', 'Date')
+    timestamp = ('TIMESTAMP', 'Date')
+    time = ('TIME', 'Date')
+    char = ('CHAR', 'String')
+    varchar = ('VARCHAR', 'String')
+    longtext = ('VARCHAR', 'String')
+
