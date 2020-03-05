a2226451f0c30484f73bbc4a2452eae8ec4727cd
xiqiuyimeng <847466569@qq.com>
2020-03-06 00:27:40 +0800

    生成mapper接口时，附加@Mapper注解

---
+++ exec_sql.sql
@@ -0,0 +1,7 @@
+
+
+SELECT id, company_name space_name, id company_count, id personal_count, id day_count, clean_amount
+rent_income, clean_amount average_income, clean_amount rent_income_after_tax, clean_amount, clean_amount
+ clean_amount_after_tax, clean_amount other, clean_amount meeting_room_income, clean_amount place_income,
+ clean_amount sum_income, clean_amount sum_income_after_tax, clean_amount income_rate
+from lease_order limit 1
