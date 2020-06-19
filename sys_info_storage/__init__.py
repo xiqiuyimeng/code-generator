3e861187087f7b6ed2a48081dc82562dede71ee1
xiqiuyimeng <847466569@qq.com>
2020-06-19 20:15:41 +0800

    增加sqlite数据库，管理系统配置

---
+++ sys_info_storage/__init__.py
@@ -0,0 +1,3 @@
+﻿# -*- coding: utf-8 -*-
+_author_ = 'luwt'
+_date_ = '2020/6/19 19:41'
---
+++ sys_info_storage/sqlite.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+import sqlite3
+_author_ = 'luwt'
+_date_ = '2020/6/19 19:41'
+
+
+DB = 'mysql_generator'
+SYS_TABLE = ''
+CONN_TABLE = 'connection'
+
+conn_sql = {
+    'create': f'''create table if not exists {CONN_TABLE}
+    (id int primary key not null,
+    name char(50) not null,
+    host char(20) not null,
+    port int not null,
+    user char(20) not null,
+    pwd char(30) not null
+    );''',
+    'insert': f'insert into {CONN_TABLE} values ()',
+    'update': f'update {CONN_TABLE} set name = "", host = "", port = 3306, user = "", pwd = "" where id = 1',
+    'delete': f'delete from {CONN_TABLE} where id = 1',
+    'select': f'select * from {CONN_TABLE}',
+}
