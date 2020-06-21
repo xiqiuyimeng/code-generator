feea907de430c1b67a16d7bec9b87efb29e8a3dc
逯文涛 <--global>
2020-06-21 16:20:52 +0800

    增加添加编辑连接窗口，增加编辑连接主窗口与子窗口联通

---
+++ message_box.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import QMessageBox, QErrorMessage
+_author_ = 'luwt'
+_date_ = '2020/6/21 16:08'
+
+
+def pop_success(msg):
+    QMessageBox.information(QMessageBox(), 'mysql', msg, QMessageBox.Ok)
+
+
+def pop_fail():...
+
+
--- sys_info_storage/sqlite.py
+++ sys_info_storage/sqlite.py
@@ -1,7 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
+from collections import namedtuple
 import sqlite3
 _author_ = 'luwt'
 _date_ = '2020/6/19 19:41'
+
+
+Connection = namedtuple('Connection', 'id name host port user pwd')
 
 
 DB = 'mysql_generator'
@@ -10,15 +14,76 @@ CONN_TABLE = 'connection'
 
 conn_sql = {
     'create': f'''create table if not exists {CONN_TABLE}
-    (id int primary key not null,
+    (id integer PRIMARY KEY autoincrement,
     name char(50) not null,
     host char(20) not null,
     port int not null,
     user char(20) not null,
     pwd char(30) not null
     );''',
-    'insert': f'insert into {CONN_TABLE} values ()',
-    'update': f'update {CONN_TABLE} set name = "", host = "", port = 3306, user = "", pwd = "" where id = 1',
-    'delete': f'delete from {CONN_TABLE} where id = 1',
+    'insert': f'insert into {CONN_TABLE}(name, host, port, user, pwd)',
+    'update': f'update {CONN_TABLE} ',
+    'delete': f'delete from {CONN_TABLE} where id = ',
     'select': f'select * from {CONN_TABLE}',
 }
+
+conn = sqlite3.connect(DB)
+cursor = conn.cursor()
+cursor.execute(conn_sql.get('create'))
+
+
+def add_conn(connection):
+    values = f' values("{connection.name}", "{connection.host}",' \
+             f' {connection.port}, "{connection.user}", "{connection.pwd}")'
+    sql = conn_sql.get('insert') + values
+    cursor.execute(sql)
+    conn.commit()
+
+
+def delete_conn(conn_id):
+    sql = conn_sql.get('delete') + f'{conn_id}'
+    cursor.execute(sql)
+    conn.commit()
+
+
+def update_conn(connection):
+    set_sql = f' set name = "{connection.name}",' \
+              f'host = "{connection.host}", ' \
+              f'port = {connection.port}, ' \
+              f'user = "{connection.user}", ' \
+              f'pwd = "{connection.pwd}"' \
+              f'where id = {connection.id}'
+    sql = conn_sql.get('update') + set_sql
+    cursor.execute(sql)
+    conn.commit()
+
+
+def get_conns():
+    cursor.execute(conn_sql.get('select'))
+    data = cursor.fetchall()
+    result = list()
+    for row in data:
+        result.append(Connection(*row))
+    return result
+
+
+def get_conn(conn_id):
+    sql = conn_sql.get('select') + f' where id = {conn_id}'
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    return Connection(*data)
+
+
+def get_new_conn():
+    sql = conn_sql.get('select') + ' order by id desc limit 1'
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    return Connection(*data)
+
+
+# connection = Connection(None, 'centos121', 'centos121', 3306, 'root', 'admin')
+# add_conn(connection)
+# conns = get_conns()
+# print(conns)
+# conn = get_new_conn()
+# print(conn)
