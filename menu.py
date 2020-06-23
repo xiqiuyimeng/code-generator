a944aa123b709161e7edccbae6c2def1e45de2f2
逯文涛 <luwentao@bonc.com.cn>
2020-06-23 17:34:21 +0800

    右键菜单完善

--- menu.py
+++ menu.py
@@ -46,16 +46,31 @@ def get_db_menu_names(item, checked):
     return menu_names
 
 
-def get_table_menu_names():
+def get_table_menu_names(table_opened, checked):
     """
     生成第三层，数据表列表的右键菜单
-
+    @:param table_opened 表是否打开
+    @:param checked 元祖：其中包含两个布尔值，第一个代表是否全选，第二个代表是否部分选中
+    菜单：
+        全选：添加取消选择菜单
+        部分选中：添加全选菜单和取消选择菜单
+        都未选中：添加全选菜单
     """
     menu_names = list()
-    menu_names.append(OPEN_TABLE_MENU)
-    menu_names.append(CLOSE_TABLE_MENU)
-    menu_names.append(SELECT_ALL_FIELD_MENU)
-    menu_names.append(UNSELECT_FIELD_MENU)
+    if table_opened:
+        menu_names.append(CLOSE_TABLE_MENU)
+        # 全选时：添加取消选择菜单
+        if checked[0]:
+            menu_names.append(UNSELECT_FIELD_MENU)
+        # 部分选中时：添加全选菜单和取消选择菜单
+        elif checked[1]:
+            menu_names.append(SELECT_ALL_FIELD_MENU)
+            menu_names.append(UNSELECT_FIELD_MENU)
+        # 都未选中时：添加全选菜单
+        else:
+            menu_names.append(SELECT_ALL_FIELD_MENU)
+    else:
+        menu_names.append(OPEN_TABLE_MENU)
     menu_names.append(GENERATE_MENU)
     return menu_names
 
