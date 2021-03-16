13c059819d66d3156d2da1053c09f1fe0012b123
逯文涛 <luwentao@bonc.com.cn>
2021-03-16 15:15:59 +0800

    更新了一些常用的jdbcType和javaType的映射关系

--- src/constant/mysql_type.py
+++ src/constant/mysql_type.py
@@ -5,23 +5,37 @@ _date_ = '2019/1/23 11:27'
 
 
 class MysqlType(Enum):
-    tinyint = ('TINYINT', 'Integer')
+    """第一个为jdbcType，第二个为javaType，根据mybatis映射关系取得"""
+    bit = ('BIT', 'Boolean')
+
+    tinyint = ('TINYINT', 'Byte')
+    smallint = ('SMALLINT', 'Short')
     int = ('INTEGER', 'Integer')
-    text = ('VARCHAR', 'String')
+    bigint = ('BIGINT', 'Long')
+
     float = ('FLOAT', 'Float')
     double = ('DOUBLE', 'Double')
+
+    real = ('REAL', 'BigDecimal')
     decimal = ('DECIMAL', 'BigDecimal')
+    numeric = ('NUMERIC', 'BigDecimal')
+
+    char = ('CHAR', 'String')
+    varchar = ('VARCHAR', 'String')
+
+    tinytext = ('VARCHAR', 'String')
+    text = ('VARCHAR', 'String')
+    mediumtext = ('LONGVARCHAR', 'String')
+    longtext = ('LONGVARCHAR', 'String')
+
     date = ('DATE', 'Date')
     datetime = ('TIMESTAMP', 'Date')
     timestamp = ('TIMESTAMP', 'Date')
     time = ('TIME', 'Date')
-    char = ('CHAR', 'String')
-    varchar = ('VARCHAR', 'String')
-    longtext = ('VARCHAR', 'String')
-    bigint = ('BIGINT', 'Long')
 
 
 # 需要import的类型和对应语句
 import_type = {
-    'Date': 'import java.util.Date;'
+    'Date': 'import java.util.Date;',
+    'BigDecimal': 'import java.math.BigDecimal;',
 }
