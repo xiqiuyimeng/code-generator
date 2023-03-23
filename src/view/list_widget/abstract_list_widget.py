35b13badadd267d086c9d3d3dac93f9d0b293b3f
xiqiuyimeng <847466569@qq.com>
2023-03-23 11:21:34 +0800

    列表控件支持拖拽元素，以调整顺序

--- src/view/list_widget/abstract_list_widget.py
+++ src/view/list_widget/abstract_list_widget.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from PyQt5.QtGui import QDropEvent
+
 from src.view.item_view_widget.abstract_item_view import AbstractItemView
 from src.view.searcher.smart_item_view import SmartSearcherListWidget
 
@@ -12,3 +14,25 @@ class AbstractListWidget(SmartSearcherListWidget, AbstractItemView):
         super().__init__(*args)
         # 设置列表项间距
         self.setSpacing(5)
+
+    def dropEvent(self, event: QDropEvent):
+        # 重写，实现拖拽效果
+        # 获取拖入事件的坐标
+        pos = event.pos()
+        # 获取当前坐标下的item
+        current_item = self.itemAt(pos)
+        # 获取该item的index
+        current_index = self.indexFromItem(current_item)
+        # 获取行数
+        current_row = current_index.row()
+
+        # 获取拖入item的父组件
+        source_widget = event.source()
+        # 获取所有拖入item
+        source_items = source_widget.selectedItems()
+        # 当前允许单选就可以了，这样减少其他地方的复杂度
+        for source_item in source_items:
+            # 在列表中移除之前的项
+            self.takeItem(self.indexFromItem(source_item).row())
+            # 插入到当前位置
+            self.insertItem(current_row, source_item)
