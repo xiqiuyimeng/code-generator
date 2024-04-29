3cbb04f3eb467c2accdfb59c15eceb97b4ea1b8a
xiqiuyimeng <847466569@qq.com>
2024-04-29 10:55:35 +0800

    初始化数据源种类时，需要找出当前的数据源类别，并加载对应的数据源列表

--- src/service/util/ds_category_util.py
+++ src/service/util/ds_category_util.py
@@ -5,7 +5,8 @@ _date_ = '2022/9/26 18:54'
 
 
 def get_current_ds_category(ds_categories):
-    current_ds_category = [ds_category for ds_category in ds_categories if ds_category.is_current]
-    if current_ds_category and len(current_ds_category) == 1:
-        return current_ds_category[0]
+    # 找出当前的数据源类别
+    current_ds_categories = [ds_category for ds_category in ds_categories if ds_category.is_current]
+    if current_ds_categories and len(current_ds_categories) == 1:
+        return current_ds_categories[0]
 
