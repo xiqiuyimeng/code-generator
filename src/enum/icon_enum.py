bc0a95eaee3be6892297c7b342b3916506b16a17
xiqiuyimeng <847466569@qq.com>
2024-07-08 07:43:44 +0800

    增加右键菜单取消选择节点icon文件，更新sql数据源，数据库节点右键菜单取消选择icon引用

--- src/enum/icon_enum.py
+++ src/enum/icon_enum.py
@@ -68,7 +68,7 @@ class IconEnum(Enum):
     cancel_open_db_icon = CANCEL_OPEN_DB_ACTION, 'icon:cancel.png'
     close_db_icon = CLOSE_DB_ACTION, 'icon:close.png'
     select_all_tb_icon = SELECT_ALL_TB_ACTION, 'icon:checked.png'
-    unselect_tb_icon = UNSELECT_TB_ACTION, 'icon:exec.png'
+    unselect_tb_icon = UNSELECT_TB_ACTION, 'icon:unchecked.png'
     refresh_db_icon = REFRESH_DB_ACTION, 'icon:refresh.png'
     cancel_refresh_db_icon = CANCEL_REFRESH_DB_ACTION, 'icon:cancel.png'
     # sql tb
