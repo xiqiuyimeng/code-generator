b72702192b742e1b31a95836c2525e9eff5a882f
xiqiuyimeng <847466569@qq.com>
2024-07-07 17:44:32 +0800

    增加右键菜单全选节点icon文件，更新项目中结构体数据源，文件夹节点全选所有节点icon引用

--- src/enum/icon_enum.py
+++ src/enum/icon_enum.py
@@ -86,7 +86,7 @@ class IconEnum(Enum):
     del_struct_icon = DEL_STRUCT_ACTION, 'icon:remove.png'
     refresh_struct_icon = REFRESH_STRUCT_ACTION, 'icon:refresh.png'
     cancel_refresh_struct_icon = CANCEL_REFRESH_STRUCT_ACTION, 'icon:cancel.png'
-    select_all_icon = SELECT_ALL_ACTION, 'icon:remove.png'
+    select_all_icon = SELECT_ALL_ACTION, 'icon:checked.png'
     unselect_icon = UNSELECT_ACTION, 'icon:remove.png'
     refresh_folder_icon = REFRESH_FOLDER_ACTION, 'icon:refresh.png'
     cancel_refresh_folder_icon = CANCEL_REFRESH_FOLDER_ACTION, 'icon:cancel.png'
